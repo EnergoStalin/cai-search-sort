02ec12c3a6916f05f8ccdd00a7ea94e6fe1ec4ff
EnergoStalin <ens.stalin@gmail.com>
2024-10-07 00:25:52 +0300

    chore(release): 1.0.3

--- index.user.js
+++ index.user.js
@@ -3,11 +3,12 @@
 // @author        EnergoStalin
 // @description   Sort search so cards with public definition stays on top and marked with a star
 // @license       AGPL-3.0-only
-// @version       1.0.2
+// @version       1.0.3
 // @namespace     https://c.ai
 // @match         https://character.ai/search*
 // @run-at        document-body
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=character.ai
+// @grant         GM.addStyle
 // ==/UserScript==
 
 (async () => {
@@ -53,6 +54,27 @@ async function getCharacterInfo(id) {
 }
 __name(getCharacterInfo, "getCharacterInfo");
 
+// src/styles/tooltip.css
+GM.addStyle(`
+.tooltip {
+  position: relative;
+  cursor: pointer;
+}
+.tooltip .tooltiptext {
+  visibility: hidden;
+  text-align: left;
+  z-index: 1;
+  opacity: 0;
+  transition: opacity 0.3s;
+  font-size: 0.7em;
+  color: #626262;
+}
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+  opacity: 1;
+}
+`);
+
 // src/icons.ts
 var starredIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#75FB4C"><path d="M371.01-324 480-390.22 589-324l-29-124 97-84-127-11-50-117-50 117-127 11 96.89 83.95L371.01-324ZM480-72 360-192H192v-168L72-480l120-120v-168h168l120-120 120 120h168v168l120 120-120 120v168H600L480-72Zm0-102 90-90h126v-126l90-90-90-90v-126H570l-90-90-90 90H264v126l-90 90 90 90v126h126l90 90Zm0-306Z"/></svg>';
 var pendingIcon = '<svg xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill="#5985E1"><path d="M288-420q25 0 42.5-17.5T348-480q0-25-17.5-42.5T288-540q-25 0-42.5 17.5T228-480q0 25 17.5 42.5T288-420Zm192 0q25 0 42.5-17.5T540-480q0-25-17.5-42.5T480-540q-25 0-42.5 17.5T420-480q0 25 17.5 42.5T480-420Zm192 0q25 0 42.5-17.5T732-480q0-25-17.5-42.5T672-540q-25 0-42.5 17.5T612-480q0 25 17.5 42.5T672-420ZM480.28-96Q401-96 331-126t-122.5-82.5Q156-261 126-330.96t-30-149.5Q96-560 126-629.5q30-69.5 82.5-122T330.96-834q69.96-30 149.5-30t149.04 30q69.5 30 122 82.5T834-629.28q30 69.73 30 149Q864-401 834-331t-82.5 122.5Q699-156 629.28-126q-69.73 30-149 30Zm-.28-72q130 0 221-91t91-221q0-130-91-221t-221-91q-130 0-221 91t-91 221q0 130 91 221t221 91Zm0-312Z"/></svg>';
@@ -66,7 +88,7 @@ function statusWrapper(card, status) {
   const height = card.querySelector("img")?.height;
   const d = document.createElement("div");
   d.dataset.status = status;
-  d.classList.add("flex", "items-center", "relative", "flex-row");
+  d.classList.add("flex", "items-center", "relative", "flex-row", "tooltip");
   d.style = `min-height: ${height}px; flex-direction: column;`;
   card.append(d);
   return d;
@@ -81,7 +103,7 @@ function setStarredStatus(card, label) {
 		<div class="flex w-full" style="justify-content: center;">
 			${starredIcon}
 		</div>
-		<span class="w-full">${label}</span>
+		<span class="w-full tooltiptext">${label}</span>
 	`;
 }
 __name(setStarredStatus, "setStarredStatus");
