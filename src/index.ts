32b1a96e405e06e7ed1348ece3ac09a8f5d3dbf6
EnergoStalin <ens.stalin@gmail.com>
2024-10-06 06:13:39 +0300

    top level awaits & refactoring & proper icon styles

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,16 @@
-import { CAPI } from "./api"
-import {
-	clearStatus,
-	isStarred,
-	setPendingStatus,
-	setStarredStatus,
-} from "./statuses"
+import { sort } from "./sorting/definition"
 import { waitNotNull } from "./util"
 
-waitNotNull(() =>
+const cardsContainer = (await waitNotNull(() =>
 	document
 		.evaluate(
 			"/html/body/div[1]/div/main/div/div/div/main/div/div[2]",
 			document,
 		)
 		.iterateNext(),
-).then((node) => {
-	const cardsContainer = node as HTMLElement
-	const capi = new CAPI()
+)) as HTMLElement
 
-	const cardsObserver = new MutationObserver(sortSearches)
-	function sortSearches() {
-		cardsObserver.disconnect()
-		const nodes = Array.from(
-			cardsContainer.childNodes,
-		) as unknown as HTMLElement[]
+const sortSearches: MutationCallback = (_, observer) =>
+	sort(observer, cardsContainer)
 
-		Promise.all(
-			nodes.map(async (card) => {
-				if (isStarred(card)) return
-
-				setPendingStatus(card)
-				const info = await capi.getCharacterInfo(
-					(card as HTMLLinkElement).href.split("/").pop()!,
-				)
-				clearStatus(card)
-
-				if (info.definition) {
-					setStarredStatus(
-						card,
-						`${info.description.length}/${info.definition.length}`,
-					)
-				} else {
-					cardsContainer.append(card)
-				}
-
-				return [info.definition?.length, info.description?.length]
-			}),
-		).then((e) => {
-			console.log(e)
-
-			cardsObserver.observe(cardsContainer, {
-				attributes: false,
-				childList: true,
-				subtree: false,
-			})
-		})
-	}
-
-	sortSearches()
-})
+sortSearches([], new MutationObserver(sortSearches))
